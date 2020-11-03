d6c679e0d023e07073ebace167a003e6f235ef96
NahHyun-Kim <cherish3_@naver.com>
2020-11-04 00:00:19 +0900

    second commit

---
+++ src/JavaBasic/prac_02_char.java
@@ -0,0 +1,13 @@
+package JavaBasic;
+
+public class prac_02_char {
+
+	public static void main(String[] args) {
+		// char 또는 String 타입의 사용
+		char c = 'A';
+		char c1 = 65; //일종의 정수 타입이기 때문에 65 대입 가능
+		char c2 = '\u0041';
+		char c3 = 'A'; 
+	}
+
+}
