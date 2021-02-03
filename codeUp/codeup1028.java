74eb2e1bd0337e78348b9584d9d23007d3598611
NahHyun-Kim <64064423+NahHyun-Kim@users.noreply.github.com>
2021-02-04 00:50:52 +0900

    210204 codeup2

---
+++ codeUp/codeup1028.java
@@ -0,0 +1,16 @@
+package codeUp;
+
+import java.util.Scanner;
+
+public class codeup1028 {
+
+	public static void main(String[] args) {
+		// 정수 1개를 입력받아 그대로 출력하기(unsigned int)
+		Scanner in = new Scanner(System.in);
+		
+		Long num = in.nextLong();
+		
+		System.out.println(num);
+	}
+
+}
